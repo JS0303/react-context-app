04662e5a9385b5b7d24f840d8a7a0d7172681b4d
js0303 <wlstjr0303@gmail.com>
2023-02-28 12:27:35 +0900

    에러 발생 처리

--- client/src/components/Type.js
+++ client/src/components/Type.js
@@ -2,10 +2,11 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Products from "./Products";
 import Options from "./Options";
+import ErrorBanner from "./ErrorBanner";
 
 const Type = ({ orderType }) => {
   const [items, setItems] = useState([]);
-
+  const [error, setError] = useState(false);
   useEffect(() => {
     loadItems(orderType);
   }, [orderType]);
@@ -16,7 +17,7 @@ const Type = ({ orderType }) => {
       setItems(response.data);
       console.log("response.data", response.data);
     } catch (error) {
-      console.log(error);
+      setError(true);
     }
   };
   const ItemComponent = orderType === "product" ? Products : Options;
@@ -27,6 +28,9 @@ const Type = ({ orderType }) => {
       imagePath={item.imagePath}
     />
   ));
+  if (error) {
+    return <ErrorBanner message="에러가 발생했습니다." />;
+  }
 
   return (
     <div>
